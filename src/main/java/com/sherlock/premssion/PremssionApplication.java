28902d77b4c813863024870c84499d1c0098a651
LMY <1243935366@qq.com>
2018-04-05 12:05:24 +0800

    项目启动

---
+++ src/main/java/com/sherlock/premssion/PremssionApplication.java
@@ -0,0 +1,12 @@
+package com.sherlock.premssion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremssionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremssionApplication.class, args);
+	}
+}
