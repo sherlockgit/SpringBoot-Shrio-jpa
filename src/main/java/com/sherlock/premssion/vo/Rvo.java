00ddc24b874bd8c4950500b6df30515ab8a8eccd
LMY <1243935366@qq.com>
2018-04-05 15:56:10 +0800

    编写Http返回的最外层对象以及其对象模型工具类

---
+++ src/main/java/com/sherlock/premssion/vo/Rvo.java
@@ -0,0 +1,29 @@
+package com.sherlock.premssion.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+/**
+ * Http返回的对象模型
+ * author: 小宇宙
+ * date: 2018/4/5
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Rvo<T> {
+
+    /**
+     * 状态码
+     */
+    private Integer code;
+
+    /**
+     * 提示信息
+     */
+    private String msg;
+
+    /**
+     * 具体数据
+     */
+    private T data;
+}
